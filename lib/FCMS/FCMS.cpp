8106ef16822651f35f444121841813cd43d11154
Sviatoslav <stehnii.pn@ucu.edu.ua>
2024-10-03 20:02:20 +0300

    feat: add base logic of FCMS (state changing system), designed future methods

---
+++ lib/FCMS/FCMS.cpp
@@ -0,0 +1,24 @@
+#include "FCMS.h"
+
+FCMS::FCMS()
+{
+}
+
+void FCMS::setState(STATE state)
+{
+  curr_state_ = state;
+}
+
+bool FCMS::nextState()
+{
+  if (curr_state_ < 5) {
+    curr_state_ = static_cast<STATE>(static_cast<int>(curr_state_) + 1);;
+    return true;
+  }
+  return false;
+}
+
+STATE FCMS::getState()
+{
+  return curr_state_;
+}
--- lib/FCMS/FCMS.h
+++ lib/FCMS/FCMS.h
@@ -5,3 +5,38 @@
 
 // FCMS lib
 // Flight Computer Managment System 
+enum STATE {
+  SAFE=0,
+  IDLE=1,
+  FLIGHT=2,
+  DESCENT=3,
+  LANDING=4,
+  LANDED=5
+};
+
+
+class FCMS {
+  private:
+    IMU imu_;
+    BMP280 baro_;
+    Flash flash_;
+
+    STATE curr_state_;
+  public:
+    FCMS();
+    ~FCMS() = default;
+
+    void setState(STATE state);
+    bool nextState();
+    STATE getState();
+
+    // TODO: implement below methods
+
+    // updates some variables related to IMU, barometer and gps
+    void navigate();
+    // writes data to flash memmory
+    void commit();
+    
+
+
+};
