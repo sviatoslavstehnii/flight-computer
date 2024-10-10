aa394860657604c4142df6fcc72fb8e9af283534
Sviatoslav <stehnii.pn@ucu.edu.ua>
2024-10-10 20:31:17 +0300

    feat (flash chip): rebuild everything with other lib usage, fixed partition system

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,38 @@
+#include <SPI.h>
+#include <SPIFlash.h>
+#include <Wire.h>
+#include "FCMS.h"
 
-
-#include <FCMS.h>
-
-
+Flash mem{10};
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-  SDMC sd;
+  Serial.begin(115200);
+  while(!Serial);
 
-  sd.setup();
+  mem.setup(16777216, true);
+
+  // data should end with \n and be shorter than 100
+  char data1[] = "123456";
+  char data2[] = "aboba1bboj";
+  char data3[] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor";
+
+  char readData[1000] = "";
+  mem.writeToCJ(data1, sizeof(data1));
+  mem.writeToCJ(data2, sizeof(data2));
+  mem.writeToCJ(data3, sizeof(data3));
+
 
   delay(100);
 
-  sd.write("test.txt", "hello");
+  mem.readCJ(readData, 1000);
+  Serial.println(readData);
+  Serial.println("end");
 
-  delay(100);
-
-  sd.read("test.txt");
-
-  delay(100);
-  sd.remove("test.txt");
-
-  delay(100);
-
-  if (!sd.read("test.txt")) {
-    Serial.println("file removed");
-  } 
-  
 }
 
 void loop() {
-  // nothing happens after setup
+  // Your code here
 }
+
+
+
