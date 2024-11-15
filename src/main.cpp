beb4a5e5a79c14e70df69b596bf9968d897ab545
Sviatoslav <stehnii.pn@ucu.edu.ua>
2024-11-15 23:23:03 +0200

    feat: add build flags for master/slave mode

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,14 @@ void setup() {
   Serial.begin(115200);
   while (!Serial);
   Serial.println("Starting...");
+  #ifdef MASTER_MODE
+    Serial.println("Running in MASTER mode");
+  #elif defined(SLAVE_MODE)
+    Serial.println("Running in SLAVE mode");
+  #else
+    Serial.println("No mode defined. Please set build flag!");
+  #endif
+
   Wire.begin();
   delay(200);
 
